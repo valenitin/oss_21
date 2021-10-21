6a2b49a569c8273de339eee89295a34004cfc1af
Artem Borovskyi <artemk0b2396@gmail.com>
2021-10-21 20:47:22 +0300

    add start files

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+console.log('kek')
+const a = 5;
+
+console.log('kek')
+const a = 5;
+
+console.log('kek')
+const a = 5;
+
+console.log('kek')
+const a = 5;
+
+console.log('kek')
+const a = 5;
+const b = 3;
+console.log('kek')
+const a = 5;
+const b = 3;
