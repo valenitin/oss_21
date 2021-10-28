b717ed6ea082c3baf8e13cbe41bcc1b863fb3f45
Artem Borovskyi <artemk0b2396@gmail.com>
2021-10-28 23:45:09 +0300

    add product block (need to make arrows)

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
     $('.hero__slick').slick({
-        arrows:false,
+        arrows: false,
         dots: true,
-        pauseOnHover:true,
-        dotsClass:'hero__dots',
+        pauseOnHover: true,
+        dotsClass: 'hero__dots',
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1
     });
+
+    $('.product-slider').slick({
+        arrows: true,
+        appendArrows:'.slider-arrows',
+        prevArrow:'<span class="slider-arrow"><svg width="9" height="15"><use href="./img/icons.svg#icon-arrow-left"></use></svg></span>',
+        nextArrow:'<span class="slider-arrow"><svg width="9" height="15"><use href="./img/icons.svg#icon-arrow-right"></use></svg></span>',
+        dots: true,
+        variableWidth: true,
+        dotsClass: 'products__dots',
+        slidesToShow: 2,
+        slidesToScroll: 2
+    });
 });
+
+
