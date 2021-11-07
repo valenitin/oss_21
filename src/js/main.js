ac2d1d3e2df94c4ba46423b93142caf9d26d971f
Artem Borovskyi <artemk0b2396@gmail.com>
2021-11-07 20:47:50 +0200

    change lang-select & slick-arrow

--- src/js/main.js
+++ src/js/main.js
@@ -11,16 +11,31 @@ $(document).ready(function () {
     });
 
     $('.product-slider').slick({
-        arrows: true,
+        arrows: false,
         appendArrows:'.slider-arrows',
-        prevArrow:'<span class="slider-arrow"><svg width="9" height="15"><use href="./img/icons.svg#icon-arrow-left"></use></svg></span>',
-        nextArrow:'<span class="slider-arrow"><svg width="9" height="15"><use href="./img/icons.svg#icon-arrow-right"></use></svg></span>',
+        // prevArrow:'<span class="slider-arrow"><svg width="9" height="15"><use href="./img/icons.svg#icon-arrow-left"></use></svg></span>',
+        // nextArrow:'<span class="slider-arrow"><svg width="9" height="15"><use href="./img/icons.svg#icon-arrow-right"></use></svg></span>',
         dots: true,
         variableWidth: true,
         dotsClass: 'products__dots',
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 2,
+
+
+
     });
+
+    $(".slider-arrows__next").click(function() {
+        $(".product-slider").slick('slickNext');
+    });
+
+    $(".slider-arrows__prev").click(function() {
+        $(".product-slider").slick('slickPrev');
+    });
+
+
+
+
 });
 
 
